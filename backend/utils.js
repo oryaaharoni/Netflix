baebfd39c69d1036759f2a4b29d7f79240cd5b12
ShellyAharoni <shelly9565@gmail.com>
2024-02-08 14:47:52 +0200

    user controller

---
+++ backend/utils.js
@@ -0,0 +1,5 @@
+import jwt from 'jsonwebtoken';
+
+export const generateToken = ({_id, username, email}) => {
+    return jwt.sign({_id: id, username: username, email: email}, process.env.JWT_PW, {expiresIn:'7d'})
+} 
