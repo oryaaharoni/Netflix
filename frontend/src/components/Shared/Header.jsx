e208c977f962181e479d086a9cd86b8a746147d3
oryaaharoni <oryaaharonii@gmail.com>
2024-02-17 22:18:32 +0200

    login password css

--- frontend/src/components/Shared/Header.jsx
+++ frontend/src/components/Shared/Header.jsx
@@ -2,7 +2,7 @@
 const Header = () => {
   return (
     <div>
-        <a href='https://www.google.com/'>
+        <a href='/homePage'>
           <img src="/Netflix-logo.png" width={200} alt="netflix logo"/>
         </a>
     </div>
