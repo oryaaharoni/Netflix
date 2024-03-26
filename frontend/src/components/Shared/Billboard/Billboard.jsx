e2d52513a41f66201d6199bfae537556e6c250bf
eliav29 <liavma3@gmail.com>
2024-03-26 18:13:36 +0200

    add gradient in the content page

--- frontend/src/components/Shared/Billboard/Billboard.jsx
+++ frontend/src/components/Shared/Billboard/Billboard.jsx
@@ -9,20 +9,23 @@ const Billboard = ({ item }) => {
     }
 
     return (
-        <div className='billboard'>
-            <div className='innerBillboard'>
-                <ReactPlayer url={item.trailer} muted={true} playing={true} loop={true} width="100vw" height="56.25vw" style={{pointerEvents: 'none', objectFit: "cover"}}/>
-                <div className='info'>
-                    <img src={item.imgTitle} style={{ width: '500px', height: '120px' }} />
-                    <div className='description'>
-                        {item.description}
-                    </div>
-                    <div className='links'>
-                        <a href={`/play/${item._id}`}><i className="fa fa-play" aria-hidden="true"></i><span>Play</span></a>
-                        <button><i className="fa fa-exclamation-circle" aria-hidden="true"></i><span>Info</span></button>
+        <div className="billboard-container">
+            <div className='billboard'>
+                <div className='innerBillboard'>
+                    <ReactPlayer url={item.trailer} muted={true} playing={true} loop={true} width="100vw" height="56.25vw" style={{ pointerEvents: 'none', objectFit: "cover" }} />
+                    <div className='info'>
+                        <img src={item.imgTitle} style={{ width: '500px', height: '120px' }} />
+                        <div className='description'>
+                            {item.description}
+                        </div>
+                        <div className='links'>
+                            <a href={`/play/${item._id}`}><i className="fa fa-play" aria-hidden="true"></i><span>Play</span></a>
+                            <button><i className="fa fa-exclamation-circle" aria-hidden="true"></i><span>Info</span></button>
+                        </div>
                     </div>
                 </div>
             </div>
+            <div className="player-gradient-overlay"></div>
         </div>
     )
 }
